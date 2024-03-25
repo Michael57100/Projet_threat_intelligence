bcdebc15b25da68da7e2f63e4d1d0218b1b51101
Alexandre <43032190+Felalex57@users.noreply.github.com>
2024-03-25 16:32:17 +0100

    initial commit
    generation of YARA rules and Regex to identify typosquatted popular gaming websites

---
+++ gaming_threats.py
@@ -0,0 +1,23 @@
+from ail_typo_squatting import formatOutput, runAll
+import math, os
+
+resultList = list()
+domainList = ["store.steampowered.com", "rockstargames.com", "signin.rockstargames.com", "epicgames.com",  "ubisoftconnect.com", "account.ubisoft.com",  "ea.com", "signin.ea.com", "auth.eu.shadow.tech", "shadow.tech"]
+tempList = ["store.steampowered.com/login", "epicgames.com/id/login", "microsoft.com/store", "play.google.com/store/games"]
+limit = math.inf
+formatoutput = "regex"
+pathOutput = "."
+for domain in domainList:
+
+    resultList = runAll(
+        domain=domain, 
+        limit=math.inf, 
+        formatoutput=formatoutput, 
+        pathOutput=pathOutput, 
+        verbose=False, 
+        givevariations=False,
+        keeporiginal=False
+    )
+
+    print(resultList)
+    resultList = list()
